d820c08b760d10f876ce6f010d963608acf5183d
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2024-12-28 15:14:58 -0300

    Update RACK_ENV assignment and add database cleanup in setup

--- test/controller_books_test.rb
+++ test/controller_books_test.rb
@@ -1,13 +1,18 @@
 require 'minitest/autorun'
 require 'rack/test'
 require_relative '../server'
-RACK_ENV='test'
+RACK_ENV = 'test'
 
 class BooksControllerTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
+  end
+
+  def setup
+    # Antes de cada teste, apaga todas as entradas do banco de dados
+    DB[:books].delete
   end
 
   def test_get_books
@@ -36,13 +41,13 @@ class BooksControllerTest < Minitest::Test
     post '/books', { author: 'George Orwell' }.to_json, { 'CONTENT_TYPE' => 'application/json' }
 
     assert_equal 422, last_response.status
-    assert_includes last_response.body, 'Title can\'t be blank'
+    assert_includes last_response.body, "Title can't be blank"
   end
 
   def test_post_books_invalid_without_author
     post '/books', { title: '1984' }.to_json, { 'CONTENT_TYPE' => 'application/json' }
 
     assert_equal 422, last_response.status
-    assert_includes last_response.body, 'Author can\'t be blank'
+    assert_includes last_response.body, "Author can't be blank"
   end
 end
